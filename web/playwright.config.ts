5a2d72914697a4f1dffa2aa148dab2a733a070f1
Kevin Hira <kevin@hira.nz>
2025-12-23 14:57:16 +1300

    Test playwright in GHA with 4 workers

--- web/playwright.config.ts
+++ web/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Limit workers to avoid overwhelming the test environment */
-  workers: process.env.CI ? 1 : 4,
+  workers: process.env.CI ? 4 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
